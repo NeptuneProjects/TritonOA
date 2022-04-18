6e5d5684dd5b5a01f54b549c18df3d1728811b45
NeptuneProjects <william.f.jenkins@gmail.com>
2022-04-18 00:18:22 -0700

    Fmt

--- tritonoa/plotting.py
+++ tritonoa/plotting.py
@@ -3,12 +3,53 @@
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_SSP(
+        z,
+        c,
+        boundaries=None,
+        xlabel=None,
+        ylabel=None,
+        ax=None,
+        **kwargs
+    ):
+    if ax is None:
+        ax = plt.gca()
+
+    ax.plot(c, z, **kwargs)
+    if boundaries is not None:
+        boundaries = np.atleast_1d(boundaries)
+        for boundary in boundaries:
+            ax.axhline(y=boundary, color='k', linestyle=(0, (5, 10)))
+        
+    ax.invert_yaxis()
+    ax.set_xlabel(xlabel)
+    ax.xaxis.tick_top()
+    ax.xaxis.set_label_position("top")
+    ax.set_ylabel(ylabel)
+    try:
+        if c.max() < 1500:
+            xlimhi = 1500
+        else:
+            xlimhi = 10 * np.ceil(c.max() / 10)
+        
+        xlimlo = 10 * np.floor(c.min() / 10)
+        ax.set_xlim((xlimlo, xlimhi))
+        ax.set_xticks(np.arange(xlimlo, xlimhi, 10), minor=True)
+    except:
+        pass
+    ax.grid()
+    
+    return ax
 
 
 def plot_TL_2d(
         p,
         z=None,
         r=None,
+        boundaries=None,
         title=None,
         xlabel=None,
         ylabel=None,
@@ -45,6 +86,12 @@ def plot_TL_2d(
         cmap=cmap,
         **kwargs
     )
+
+    if boundaries is not None:
+        boundaries = np.atleast_1d(boundaries)
+        for boundary in boundaries:
+            ax.axhline(y=boundary, color='w', linestyle=(0, (5, 10)))
+
     ax.invert_yaxis()
     norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
     cbar = plt.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap))
@@ -52,25 +99,6 @@ def plot_TL_2d(
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
     ax.set_title(title)
-
-    return ax
-
-
-def plot_SSP(
-        z,
-        c,
-        xlabel=None,
-        ylabel=None,
-        ax=None,
-        **kwargs
-    ):
-    if ax is None:
-        ax = plt.gca()
-
-    ax.plot(c, z)
-    ax.invert_yaxis()
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
 
     return ax
 
