be870927742e4be985e7563d7d1029ca363cfde9
NeptuneProjects <william.f.jenkins@gmail.com>
2022-04-17 21:46:20 -0700

    v update

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def setup_package():
             'pandas',
             'scipy'
         ],
-        version='0.0b0',
+        version='0.0b1',
         license='MIT',
         description="Package provides interface to acoustic modeling tools \
             of Acoustics Toolbox developed by Mike Porter."
