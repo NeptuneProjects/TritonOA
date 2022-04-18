cf75df370839d881e65e6cf69bccdb2b12e3b0ad
NeptuneProjects <william.f.jenkins@gmail.com>
2022-04-17 23:33:18 -0700

    Refactor plots

--- tritonoa/plotting.py
+++ tritonoa/plotting.py
@@ -3,28 +3,26 @@
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-# import numpy as np
+
 
 def plot_TL_2d(
         p,
         z=None,
         r=None,
-        bottom=None,
         title=None,
-        xlabel="Range [km]",
-        ylabel="Depth [m]",
-        clabel="TL [dB]",
-        figsize=(8, 6),
+        xlabel=None,
+        ylabel=None,
+        clabel=None,
         vmin=-40,
         vmax=0,
         interpolation=None,
         cmap="jet",
-        show=True,
-        **pltkwargs
+        ax=None,
+        **kwargs
     ):
-    fig = plt.figure(figsize=figsize, facecolor="w")
 
-    # levs = np.linspace(np.min(p), np.max(p), 40) / 2
+    if ax is None:
+        ax = plt.gca()
 
     if (r is not None) and (z is not None):
         extent = (
@@ -36,7 +34,7 @@ def plot_TL_2d(
     else:
         extent = None
 
-    plt.imshow(
+    ax.imshow(
         p,
         extent=extent,
         aspect="auto",
@@ -45,25 +43,36 @@ def plot_TL_2d(
         vmax=vmax,
         interpolation=interpolation,
         cmap=cmap,
-        **pltkwargs
+        **kwargs
     )
-    plt.gca().invert_yaxis()
+    ax.invert_yaxis()
     norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
     cbar = plt.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap))
     cbar.set_label(clabel)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
 
-    if show:
-        plt.show()
-        return fig
-    else:
-        return fig, plt.gca()
+    return ax
 
 
-def plot_SSP():
-    pass
+def plot_SSP(
+        z,
+        c,
+        xlabel=None,
+        ylabel=None,
+        ax=None,
+        **kwargs
+    ):
+    if ax is None:
+        ax = plt.gca()
+
+    ax.plot(c, z)
+    ax.invert_yaxis()
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+
+    return ax
 
 
 def plot_TL_1d():
