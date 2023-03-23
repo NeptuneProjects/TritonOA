def7373080ed84e5fb9a067b3bed5b1c7bd90e54
NeptuneProjects <william.f.jenkins@gmail.com>
2023-03-23 02:15:39 -0700

    Update & simplify

--- setup.py
+++ setup.py
@@ -2,43 +2,29 @@
 
 from setuptools import setup
 
+
 def setup_package():
     setup(
-        name='TritonOA',
-        url='https://github.com/NeptuneProjects/TritonOA',
-        author='William F. Jenkins II',
-        author_email='wjenkins@ucsd.edu',
-        packages=[
-            'tritonoa',
-            # 'tritonoa.core',
-            # 'tritonoa.kraken',
-            # 'tritonoa.plotting',
-            # 'tritonoa.profiles',
-            # 'tritonoa.sp'
-        ],
-        # scripts=['RISCluster/runDEC'],
-        # entry_points = {
-        #     'console_scripts': [
-        #         'query_H5size=RISCluster.utils:query_H5size',
-        #         'ExtractH5Dataset=RISCluster.utils:ExtractH5Dataset',
-        #         'GenerateSampleIndex=RISCluster.utils:GenerateSampleIndex'
-        #     ]
-        # },
+        name="TritonOA",
+        url="https://github.com/NeptuneProjects/TritonOA",
+        author="William F. Jenkins II",
+        author_email="wjenkins@ucsd.edu",
+        packages=["tritonoa"],
         install_requires=[
-            'cmocean',
-            'feather-format',
-            'jupyterlab',
-            'matplotlib',
-            'numpy',
-            'pandas',
-            'scipy'
+            "cmocean",
+            "feather-format",
+            "jupyterlab",
+            "matplotlib",
+            "numpy",
+            "pandas",
+            "scipy",
         ],
-        version='0.0b2',
-        license='MIT',
-        description="Package provides interface to acoustic modeling tools \
-            of Acoustics Toolbox developed by Mike Porter."
+        version="0.0.2",
+        license="MIT",
+        description="Package provides interface to ocean acoustic modeling and \
+            analysis tools.",
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup_package()
