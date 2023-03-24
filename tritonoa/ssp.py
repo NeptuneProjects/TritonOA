49d15e160448dbba9739aafbbabbff6a665833c3
NeptuneProjects <william.f.jenkins@gmail.com>
2023-03-24 10:35:25 -0700

    Refactor ssp classes

--- tritonoa/ssp.py
+++ tritonoa/ssp.py
@@ -1,14 +1,50 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
 import numpy as np
 
+
+class ProfileArrayLengthError(Exception):
+    pass
+
+
 @dataclass
-class SoundSpeedProfile:
+class SoundSpeedProfile(ABC):
     z: np.ndarray
     c_p: np.ndarray
+
+    @abstractmethod
+    def __post_init__(self):
+        """Post-initialization hook."""
+
+    def format_data(self):
+        self.enforce_ndarrays()
+        self.check_dimensions()
+        self.form_ndarrays()
+
+    def check_dimensions(self) -> None:
+        for k, v in vars(self).items():
+            if (len(v) > len(self.z)) or ((len(v) != 1) and (len(v) != len(self.z))):
+                raise ProfileArrayLengthError(
+                    f"Mismatch in number of elements in attribute `{k}` and `z`."
+                )
+
+    def enforce_ndarrays(self) -> None:
+        [
+            setattr(self, k, np.atleast_1d(np.array(v, dtype=np.float64)))
+            for k, v in vars(self).items()
+        ]
+
+    def form_ndarrays(self) -> None:
+        n = len(self.z)
+        [
+            setattr(self, k, np.array([v[i] if len(v) > 1 else v[0] for i in range(n)]))
+            for k, v in vars(self).items()
+            if k != "z"
+        ]
 
 
 def munk_ssp(z_max: float, dz: float = 1.0) -> dict:
