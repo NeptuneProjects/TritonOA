855a8c1f6ae439b2572b1ec47b3ae4661c90c951
NeptuneProjects <william.f.jenkins@gmail.com>
2023-04-01 22:44:44 -0700

    Add option not to compute fvec

--- tritonoa/sp/processing.py
+++ tritonoa/sp/processing.py
@@ -16,7 +16,7 @@ class NotImplementedWarning(Warning):
 def find_freq_bin(
     fvec: np.ndarray,
     X: np.ndarray,
-    f0: float,
+    freq: float,
     lower_bw: float = 1.0,
     upper_bw: float = 1.0,
 ) -> int:
@@ -24,8 +24,8 @@ def find_freq_bin(
     upper/lower bandwidths, returns the index of the frequency bin that
     contains the maximum energy.
     """
-    f_lower = f0 - lower_bw
-    f_upper = f0 + upper_bw
+    f_lower = freq - lower_bw
+    f_upper = freq + upper_bw
     ind = (fvec >= f_lower) & (fvec < f_upper)
     data = np.abs(X).sum(axis=1) / X.shape[1]
     data[~ind] = -2009
@@ -37,18 +37,21 @@ def generate_complex_pressure(
     fs: float,
     num_segments: int,
     nfft: int,
-    f0: float,
+    freq: float,
     lower_bw: float = 1.0,
     upper_bw: float = 1.0,
+    fvec: Optional[np.ndarray] = None,
     noverlap: Optional[int] = None,
     window: Optional[Callable] = None,
-) -> tuple[np.ndarray, int]:
+) -> tuple[np.ndarray, float, int]:
     if noverlap is not None:
         raise NotImplementedWarning("Overlapping segments is not implemented yet.")
 
+    if not fvec:
+        fvec = frequency_vector(fs, nfft)
+
     M = data.shape[1]
     samplers_per_segment = data.shape[0] // num_segments
-    fvec = frequency_vector(fs, nfft)
 
     complex_pressure = np.zeros((num_segments, M), dtype=complex)
     for i in range(num_segments):
@@ -60,7 +63,7 @@ def generate_complex_pressure(
             segment = segment * window(nfft)
 
         X = fft(segment, n=nfft, axis=0)
-        fbin = find_freq_bin(fvec, complex_pressure, f0, lower_bw, upper_bw)
+        fbin = find_freq_bin(fvec, complex_pressure, freq, lower_bw, upper_bw)
         complex_pressure[i] = X[fbin]
 
-    return complex_pressure, fbin
+    return complex_pressure, fvec[fbin], fbin
