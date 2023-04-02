dbc101f0075940cf2aa13f303942544e9cca3eeb
NeptuneProjects <william.f.jenkins@gmail.com>
2023-04-01 21:00:39 -0700

    Add datastream class & handler protocol

---
+++ tritonoa/data.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from dataclasses import dataclass
+import os
+from typing import Any, Optional, Protocol, Union
+
+import numpy as np
+
+
+@dataclass
+class DataStream:
+    """Contains acoustic data and time vector."""
+
+    X: Optional[np.ndarray] = None
+    t: Optional[np.ndarray] = None
+
+    def load(self, filename: Union[str, bytes, os.PathLike]) -> None:
+        """Loads data from numpy file."""
+        data = np.load(filename)
+        self.X = data.get("X", None)
+        self.t = data.get("t", None)
+
+    def save(self, filename: Union[str, bytes, os.PathLike]) -> None:
+        """Saves data to numpy file."""
+        np.savez(filename, X=self.X, t=self.t)
+
+
+class DataHandler(Protocol):
+    def convert_to_numpy(self) -> Any:
+        """Converts data to numpy arrays."""
+        ...
+
+    def load_merged(self) -> Any:
+        """Loads merged numpy file."""
+        ...
+
+    def merge_numpy_files(self) -> Any:
+        """Merges numpy files."""
+        ...
