8152a9b4da3f4ad99bf50e4b8d6ea0dea0df0a40
NeptuneProjects <william.f.jenkins@gmail.com>
2023-08-09 22:01:34 -0700

    Resolves #3

--- tritonoa/sp/mfp.py
+++ tritonoa/sp/mfp.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from enum import Enum
 from typing import Iterable, Union
 
 import numpy as np
 
 from tritonoa.sp.beamforming import beamformer
+
+
+class MultiFrequencyMethods(Enum):
+    """Enum for the different methods to combine the beamformer responses
+    for multiple frequencies."""
+
+    MEAN = "mean"
+    SUM = "sum"
+    PRODUCT = "product"
 
 
 class MatchedFieldProcessor:
@@ -18,12 +28,14 @@ class MatchedFieldProcessor:
         freq: Union[float, Iterable[float]],
         parameters: Union[dict, list[dict]],
         beamformer: callable = beamformer,
+        multifreq_method: str = "mean",
     ):
         self.runner = runner
         self.covariance_matrix = covariance_matrix
         self.freq = freq
         self.parameters = self._merge(parameters)
         self.beamformer = beamformer
+        self.multifreq_method = MultiFrequencyMethods(multifreq_method)
 
     def __call__(self, parameters: dict) -> Union[np.ndarray, complex]:
         return self.evaluate(parameters)
@@ -34,7 +46,12 @@ class MatchedFieldProcessor:
             replica_pressure = self.runner(self.parameters | {"freq": f} | parameters)
             bf_response.append(self.beamformer(k, replica_pressure))
 
-        return np.mean(np.array(bf_response), axis=0)
+        if self.multifreq_method == MultiFrequencyMethods.MEAN:
+            return np.mean(np.array(bf_response), axis=0)
+        elif self.multifreq_method == MultiFrequencyMethods.SUM:
+            return np.sum(np.array(bf_response), axis=0)
+        elif self.multifreq_method == MultiFrequencyMethods.PRODUCT:
+            return np.prod(np.array(bf_response), axis=0)
 
     @staticmethod
     def _merge(parameters: Union[dict, list[dict]]) -> dict:
