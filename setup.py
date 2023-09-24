12a44981d3d4bde90467338c1e47d48f85bd6e66
NeptuneProjects <william.f.jenkins@gmail.com>
2023-09-24 08:07:38 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def setup_package():
             "scipy",
             "tqdm"
         ],
-        version="0.0.7",
+        version="0.0.8",
         license="MIT",
         description="Package provides interface to ocean acoustic modeling and \
             analysis tools.",
